660ef518642e7d680dc9cd89a2aa13bc07f19344
hippothomas <hippolyte.thomas1@gmail.com>
2019-10-30 16:39:42 +0100

    Création du menu des items en combat et ajout de l'utilisation des items

--- bo/Player.py
+++ bo/Player.py
@@ -7,3 +7,27 @@ class Player:
     def __init__(self, username):
         self.username = username
 
+    def getInventaire(self):
+        return self.item_list
+
+    def setInventaire(self, list):
+        self.item_list = list
+
+    def addInventaire(self, item):
+        self.item_list.extend(item)
+
+    def existInventaire(self, id):
+        if 0 < id <= len(self.item_list):
+            return True
+        else:
+            return False
+
+    # Utilise un des item de l'inventaire
+    def useInventaire(self, id):
+        self.item_list[id][0] = self.item_list[id][0] - 1
+        if self.item_list[id][0] == 0:
+            self.removeFromInventaire(id)
+
+    # Permet de supprimer un item de la l'inventaire
+    def removeFromInventaire(self, id):
+        self.item_list.remove(self.item_list[id])
