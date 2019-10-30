9c7de00fcf3923d038ab506c382d6a4e1756dff3
Bastien@MSI <chauveaubastien@gmail.com>
2019-10-30 15:15:54 +0100

    menuPokedex

--- bo/Player.py
+++ bo/Player.py
@@ -3,9 +3,11 @@ class Player:
     poke_list = []
     item_list = []
     pokedex = []
+    monnaie = 0
 
     def __init__(self, username):
         self.username = username
+        self.monnaie = 500
 
     def addPokeList(self, pokemon):
         self.poke_list.append(pokemon)
